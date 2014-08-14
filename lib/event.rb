0b0a5d39ea4d71b1c85939dd284fadb9e994d0d2
Blake Mason & Christian Danielsen <student@epicodus.com>
2014-08-14 15:33:02 -0700

    list events from previous/following day/week/month

--- lib/event.rb
+++ lib/event.rb
@@ -6,13 +6,15 @@ class Event < ActiveRecord::Base
     Event.all.where("? < end_date AND ? > end_date", Time.now.strftime('%Y-%m-%d %H:%M'), (Time.now + 1.year).strftime('%Y-%m-%d %H:%M'))
   end
 
-  def self.by_date date
+  def self.by_day date
     y = date.split('-')[0].to_i
     m = date.split('-')[1].to_i
     d = date.split('-')[2].to_i
-    date_time = Time.new(y, m, d)
-    @current_day = date_time
-    Event.all.where("start_date <= ? AND end_date >= ?", date, date)
+    start_of_day = Time.new(y, m, d)
+    end_of_day =  Time.new(y, m, d, 23, 59)
+    @current_day = start_of_day
+    #Event.all.where("start_date <= ? AND end_date >= ?", date, date)
+    Event.all.where.not("start_date < ? AND end_date < ? OR start_date > ? AND end_date > ?", start_of_day, start_of_day, end_of_day, end_of_day)
   end
 
   def self.by_week date
@@ -37,8 +39,30 @@ class Event < ActiveRecord::Base
     Event.all.where.not("start_date < ? AND end_date < ? OR start_date > ? AND end_date > ?", first, first, last, last)
   end
 
+  def self.next_day
+    next_day = @current_day + 1.day
+    Event.by_day((next_day).strftime('%Y-%m-%d %H:%M'))
+  end
+
+  def self.next_week
+    Event.by_week((@current_day + 1.week).strftime('%Y-%m-%d %H:%M'))
+  end
+
   def self.next_month
     Event.by_month((@current_day + 1.month).strftime('%Y-%m-%d %H:%M'))
+  end
+
+  def self.previous_day
+    next_day = @current_day - 1.day
+    Event.by_day((next_day).strftime('%Y-%m-%d %H:%M'))
+  end
+
+  def self.previous_week
+    Event.by_week((@current_day - 1.week).strftime('%Y-%m-%d %H:%M'))
+  end
+
+  def self.previous_month
+    Event.by_month((@current_day - 1.month).strftime('%Y-%m-%d %H:%M'))
   end
 
 end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -27,11 +27,11 @@ describe 'Event' do
     end
   end
 
-  describe '.by_date' do
+  describe '.by_day' do
     it 'lists all events on a given date' do
       event1 = Event.create({description: 'Tech-crawl', start_date: '2014-08-10 20:00', end_date: '2014-08-16 20:00'})
       event2 = Event.create({description: 'MFNW', start_date: '2014-08-15', end_date: '2014-08-15'})
-      expect(Event.by_date('2014-8-12')).to eq [event1]
+      expect(Event.by_day('2014-8-12')).to eq [event1]
     end
   end
 
@@ -60,7 +60,7 @@ describe 'Event' do
   end
 
   describe '.next_month' do
-    it 'lists all events within a month' do
+    it 'lists all events within the next month' do
       before_before = Event.create({description: 'PDXPop', start_date: '2014-07-01', end_date: '2014-07-02 12:00'})
       before_during = Event.create({description: 'MFNW', start_date: '2014-07-09', end_date: '2014-08-13'})
       during_during = Event.create({description: 'Tech-crawl', start_date: '2014-08-12 20:00', end_date: '2014-08-31 20:00'})
@@ -68,7 +68,30 @@ describe 'Event' do
       after_after = Event.create({description: 'PDXPop', start_date: '2014-09-20', end_date: '2014-09-23 12:00'})
       before_after = Event.create({description: 'PDXPop', start_date: '2014-07-06', end_date: '2014-09-23 12:00'})
       Event.by_month('2014-8-12')
-      expect(Event.next_month).to eq [during_after, after_after, before_after] #8/01 - 8/31
+      expect(Event.next_month).to eq [during_after, after_after, before_after] #9/01 - 9/31
+    end
+  end
+
+  describe '.next_week' do
+    it 'lists all events within the next week' do
+      before_before = Event.create({description: 'PDXPop', start_date: '2014-08-01', end_date: '2014-08-02 12:00'})
+      before_during = Event.create({description: 'MFNW', start_date: '2014-08-09', end_date: '2014-08-13'})
+      during_during = Event.create({description: 'Tech-crawl', start_date: '2014-08-12 20:00', end_date: '2014-08-15 20:00'})
+      during_after = Event.create({description: 'PDXPop', start_date: '2014-08-15', end_date: '2014-08-23 12:00'})
+      after_after = Event.create({description: 'PDXPop', start_date: '2014-08-20', end_date: '2014-08-21 12:00'})
+      before_after = Event.create({description: 'PDXPop', start_date: '2014-08-06', end_date: '2014-08-21 12:00'})
+      Event.by_week('2014-8-12')
+      expect(Event.next_week).to eq [during_after, after_after, before_after] #8/17 - 8/23
+    end
+  end
+
+  describe '.next_day' do
+    it 'lists all events tomorrow' do
+      before_after = Event.create({description: 'Tech-crawl', start_date: '2014-08-10 20:00', end_date: '2014-08-16 20:00'})
+      after_after = Event.create({description: 'MFNW', start_date: '2014-08-15', end_date: '2014-08-17'})
+      during_after = Event.create({description: 'other', start_date: '2014-08-13 20:00', end_date: '2014-08-16 20:00'})
+      Event.by_day('2014-8-12')
+      expect(Event.next_day).to eq [before_after, during_after] #8/13
     end
   end
 end
